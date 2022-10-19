413c613f9e99be3e9fc52f212e7a33a60b90afc3
Ather1230 <ather.biswas0057@gmail.com>
2022-10-19 23:14:10 +0600

    login and sign up form added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is log in page</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" placeholder='Enter your email' required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" placeholder='Enter your password' required/>
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>New to Ema-jhon? <Link to='/signup'>SignUp Now</Link> </p>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>This is signup page</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>SignUp</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" placeholder='Enter your email' required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" placeholder='Enter your password' required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <input type="password" name="confirm-password" placeholder='Enter your confirm password' required/>
+                </div>
+                <input className='btn-submit' type="submit" value="SignUp" />
+            </form>
+            <p>Already have an account? <Link to='/login'>Login</Link> </p>
         </div>
     );
 };
