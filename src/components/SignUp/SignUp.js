a31acc1066666f3d3a7d95149e0a7e35f2b62473
Ather1230 <ather.biswas0057@gmail.com>
2022-10-20 00:24:33 +0600

    user signup and login

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/UserContext';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [error, setError] = useState(null);
+    //import context for sign up
+    const {createUser} = useContext(AuthContext);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirmPass = form.confirmPass.value;
+        //password length check...
+       if(password.length < 6){
+            setError('Password should be at least 6 character or more.');
+            return;
+       }
+       // password validation check...
+       if(password !== confirmPass){
+            setError('Your password did not match.');
+            return;
+       }
+       createUser(email, password)
+            .then( result => {
+                    const user = result.user;
+                    console.log(user);
+                    form.reset();
+            })
+            .catch( error => console.error(error))
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>SignUp</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" placeholder='Enter your email' required/>
@@ -16,9 +44,10 @@ const SignUp = () => {
                     <input type="password" name="password" placeholder='Enter your password' required/>
                 </div>
                 <div className="form-control">
-                    <label htmlFor="confirm-password">Confirm Password</label>
-                    <input type="password" name="confirm-password" placeholder='Enter your confirm password' required/>
+                    <label htmlFor="confirmPass">Confirm Password</label>
+                    <input type="password" name="confirmPass" placeholder='Enter your confirm password' required/>
                 </div>
+                <p className='text-error'>{error}</p>
                 <input className='btn-submit' type="submit" value="SignUp" />
             </form>
             <p>Already have an account? <Link to='/login'>Login</Link> </p>
--- src/components/context/UserContext.js
+++ src/components/context/UserContext.js
@@ -1,10 +1,28 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from 'firebase/auth';
+import app from '../../firebase/firebase.config';
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
+//authentication part auth
+const auth = getAuth(app);
 
 const UserContext = ({children}) => {
+    //create state for user
+    const [user, setUser] = useState(null);
+    //create user with email and password 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    //sign in user with email and password
+    const signInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // log out user form the website
+    const logOut = () => {
+        return signOut(auth)
+    }
 
-    const authInfo = {}
+    const authInfo = {user, createUser, signInUser, logOut};
 
     return (
         <AuthContext.Provider value={authInfo}>
